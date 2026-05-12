5c9572af928dc8b30c66b07364d4f18b53d28c8e
Emre Gundogdu <emreguendogdu@yandex.com>
2026-05-12 13:48:28 +0700

    fix(style): Stack vertically instead of horizontally for mobile

--- src/app/emr-demo/custom-blocks/authv2/NameInformationBlock.tsx
+++ src/app/emr-demo/custom-blocks/authv2/NameInformationBlock.tsx
@@ -313,7 +313,7 @@ const NameInformationRenderer = React.forwardRef<
         Let's start with some basic information.
       </h2>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div className="flex flex-col gap-2">
           <Label
             htmlFor={`${block.fieldName}-firstName`}
